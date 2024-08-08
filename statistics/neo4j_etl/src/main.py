63e1a7d2ac8812a86813e7597dd8567a82db80d3
Joseph <philippdeutsch@icloud.com>
2024-08-08 11:34:09 +0000

    updated neo4j with additional query and writing options

--- statistics/neo4j_etl/src/main.py
+++ statistics/neo4j_etl/src/main.py
@@ -8,6 +8,7 @@ from retry import retry
 load_dotenv()
 
 BTCUSD_CSV_PATH = os.getenv("BTCUSD_CSV_PATH")
+ETHUSD_CSV_PATH = os.getenv("ETHUSD_CSV_PATH")
 NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
@@ -20,7 +21,7 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-NODES = ["BTCUSD"]
+NODES = ["BTCUSD", "ETHUSD"]
 
 def _set_uniqueness_constraints(tx, node):
     query = f"""CREATE CONSTRAINT IF NOT EXISTS FOR (n:{node})
@@ -28,35 +29,34 @@ def _set_uniqueness_constraints(tx, node):
     _ = tx.run(query, {})
 
 @retry(tries=100, delay=10)
-def load_btcusd_graph_from_csv() -> None:
-    """Load structured BTCUSD CSV data into Neo4j"""
-    #uri = "bolt://localhost:7687"
-    #driver = GraphDatabase.driver(uri, auth=("neo4j", "Password"))
+def load_graph_from_csv(csv_path, node_label) -> None:
+    """Load structured CSV data into Neo4j"""
     driver = GraphDatabase.driver(
         NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD)
     )
 
-    LOGGER.info("Setting uniqueness constraints on nodes")
+    LOGGER.info(f"Setting uniqueness constraints on {node_label} nodes")
     with driver.session(database="neo4j") as session:
-        for node in NODES:
-            session.execute_write(_set_uniqueness_constraints, node)
+        session.execute_write(_set_uniqueness_constraints, node_label)
 
-    LOGGER.info("Loading BTCUSD nodes")
+    LOGGER.info(f"Loading {node_label} nodes")
     with driver.session(database="neo4j") as session:
         query = f"""
         LOAD CSV WITH HEADERS
-        FROM '{BTCUSD_CSV_PATH}' AS row
-        MERGE (b:BTCUSD {{
-            date: row.Date,
-            open: toFloat(row.Open),
-            high: toFloat(row.High),
-            low: toFloat(row.Low),
-            close: toFloat(row.Close),
-            adj_close: toFloat(row["Adj Close"]),
-            volume: toFloat(row.Volume)
-        }});
+        FROM '{csv_path}' AS row
+        MERGE (b:{node_label} {{ date: row.Date }})
+        ON CREATE SET
+            b.open = toFloat(row.Open),
+            b.high = toFloat(row.High),
+            b.low = toFloat(row.Low),
+            b.close = toFloat(row.Close),
+            b.adj_close = toFloat(row["Adj Close"]),
+            b.volume = toFloat(row.Volume)
+        ON MATCH SET
+            b = b;  // No operation on match
         """
         _ = session.run(query, {})
 
 if __name__ == "__main__":
-    load_btcusd_graph_from_csv()
+    load_graph_from_csv(BTCUSD_CSV_PATH, "BTCUSD")
+    load_graph_from_csv(ETHUSD_CSV_PATH, "ETHUSD")
